6f65bd5406a2bae7ac555721caa7e2d5d1d1aeb7
Sachin <145639140+Derikklok@users.noreply.github.com>
2025-12-02 23:10:15 +0530

    Add initial project setup with .env configuration and main application logic

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main(){
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+	fmt.Println(PORT)
+	fmt.Println("Hello World!")	
+}
